f9848f226a637bd3b9d3ac7c43e04936a3a7a002
ngattusohw <ngattusohw@gmail.com>
2017-10-23 23:04:59 -0400

    Adding first problem implemntation

---
+++ Assembly1/Assembly1/main.asm
@@ -0,0 +1,31 @@
+;
+; Assembly1.asm
+;
+; Created: 10/23/2017 10:55:40 PM
+; Author : donna
+;
+
+; Write an assembly language program to add up the numbers from 1 to 15
+;(inclusive) and output the result to Port B (Points 15). 
+
+
+.include "m328Pdef.inc"
+init:
+	.def max=r16
+	.def counter=r17
+	.def sum=r18
+	
+main:
+	ldi max,15
+	ldi counter,1
+	clr sum
+	
+loop:
+	cp max,counter
+	brlo end
+	add sum,counter
+	inc counter
+	rjmp loop
+	
+end: 
+	out PORTB,sum
